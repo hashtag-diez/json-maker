1599ed95c44bbb0bc4874a057efc908ff36bc1ca
Diez <62673882+zeidfazazi@users.noreply.github.com>
2022-06-18 20:54:50 +0200

    Deleted code that has been transfered in the utils folder

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,79 +1,25 @@
 <script lang="ts">
   import type { PreviewType } from "src/types/Preview.type";
   import { ValueType } from '../types/Preview.type'
+  import { getLastParent, updateListPreview, insertChildPreview } from "../utils/EditorMethods"
+  import Fa from 'svelte-fa/src/fa.svelte'
+  import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons/index.es'
+
   export let listPreview: PreviewType[];
   export let id: number;
   let edited: boolean = false
   let name: string = ""
-  let value: string | PreviewType[] = null
+  let value: string | PreviewType[] = ""
   let type: string = ""
   let hasParent: boolean = false
-  const associationStringValueType: Record<string, ValueType> = {
-    String: ValueType.STRING,
-    Number: ValueType.NUMBER,
-    Boolean: ValueType.BOOLEAN,
-    Array: ValueType.ARRAY,
-    Object: ValueType.OBJECT
-  }
-  const getLastParent = (listPreview: PreviewType[]) => {
-    let res = -1
-    listPreview.forEach(item => {
-      if(item.type == ValueType.OBJECT){
-        const childNodes = item.value as PreviewType[]
-        const innerParentId = getLastParent(childNodes)
-        res = (innerParentId == -1 ? item.id : innerParentId)
-      }
-    })
-    console.log(res)
-    return res
-  }
-  function insertChildPreview (listPreview: PreviewType[], item: PreviewType): boolean {
-    let inserted = false
-    for( let i = 0; i < listPreview.length; i++){
-      const node = listPreview[i]
-      if(node.type == ValueType.OBJECT){
-        const nodePreviewList = node.value as PreviewType[]
-        if(node.id == item.parentId){
-          nodePreviewList.push(item)
-          inserted = true
-          break;
-        } else {
-          inserted = insertChildPreview(nodePreviewList, item)
-          if(inserted){
-            break;
-          }
-        }        
-      }
-    }
-    return inserted
-  }
-  const updateListPreview = () => {
-    const oldListPreview = listPreview
-    let parentId = null
-    let newItem: PreviewType = {
-        id: id,
-        label: name,
-        type: associationStringValueType[type],
-        value: (associationStringValueType[type] == ValueType.OBJECT ? [] :  value),
-        parentId: parentId
-      }
-    if(hasParent){
-      newItem.parentId = getLastParent(oldListPreview)
-      insertChildPreview(oldListPreview, newItem)
-    } else {
-      oldListPreview.push({
-        id: id,
-        label: name,
-        type: associationStringValueType[type],
-        value: (associationStringValueType[type] == ValueType.OBJECT ? [] :  value),
-        parentId: parentId
-      })
-    }
+
+  const handleNewItem = () => {
+    const oldListPreview = updateListPreview(hasParent, listPreview, id, name, type, value)
     listPreview = oldListPreview
     edited = true
   }
 </script>
-<div style="display: flex; gap: 10px">
+<form style="display: flex; gap: 10px">
   <input bind:value={name} type="text" name="" placeholder="Name" id="">
   <div>
     <input bind:checked={hasParent} type="checkbox" name="hasParent" id="">
@@ -87,10 +33,68 @@
     <option value="Array">Array</option>
     <option value="Boolean">Boolean</option>
   </select>
-  {#if type !== 'Object'} 
+  {#if type === ""} 
+    <input bind:value={value} type="text" placeholder="Value" name="" id="" disabled>
+  {:else if (type === "String" || type === "Array") }
     <input bind:value={value} type="text" placeholder="Value" name="" id="">
+  {:else if (type === "Number")}
+    <input bind:value={value} type="number" placeholder="Value" name="" id="">
+  {:else if (type === "Boolean")}
+    <select bind:value={value} name="" id="">
+      <option value="">Value</option>
+      <option value={true} selected>true</option>
+      <option value={false}>false</option>  
+    </select>    
   {/if}  
   {#if !edited && name!=="" && (type === "Object" || (type !== "Object" && value!=="")) && type!==""}
-    <button on:click={() => updateListPreview()}>GO</button>
+    <div class="validate" on:click={() => handleNewItem()}>
+      <Fa 
+        icon={faCheck} 
+        color="#ffffff"  
+      />
+    </div>
   {/if}  
-</div>
+  <Fa 
+    icon={faXmark} 
+    color="#95a5a6"  
+  />  
+</form>
+
+<style>
+  form{
+    background: #ffffff;
+    padding: 7px;
+    margin: 10px 0px;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+  }
+  form input:first-child{
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px
+  }
+  input[type="text"], select{
+    font-family: 'Fredoka', sans-serif;
+    padding: 10px;
+    background-color: #F1F5FD;
+    border: 0;
+    color: #34495e;
+    padding-left: 12px;
+    font-size: 16px;
+  }
+  input[type="text"]:focus, select:focus{
+    background-color: #E1E8F6;
+    outline: none;
+  }
+  input[type="text"]::placeholder{
+    color: #95a5a6;
+  }
+  .validate{
+    display: grid;
+    place-items: center;
+    padding: 10px 15px;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+    background-color: #367AFC;
+  }
+</style>
